1147cf7633fbf511df062051b33364393e40c211
Christian <Christian>
2025-04-15 19:57:20 +0300

    created games and categories

--- application/app-infra/lambdas/Api/src/mock/gameCategories.ts
+++ application/app-infra/lambdas/Api/src/mock/gameCategories.ts
@@ -1,20 +1,35 @@
 export const gameCategories = [
   {
     categoryId: "sport",
-    displayName: "Sports",
-    description: "All about football, basketball, tennis, ice hockey, etc.",
-    image: "",
+    displayName: "Sportti Visat",
+    description:
+      "Hauska ja haastava visasarja, joka kattaa eri urheilulajit jääkiekosta aina moottoriurheiluun. Jokaisessa pelissä on useita kysymyksiä",
+    image: "sport-quiz.jpg",
+    logo: "",
+    headerImage: "",
   },
   {
     categoryId: "bingo",
     displayName: "Bingo",
-    description: "All Bingos, fast, slow and everything between.",
-    image: "",
+    description:
+      "Nopeatempoinen musiikkibingo, jossa numerot on korvattu kappaleilla.",
+    image: "jukebox-bingo.jpg",
+    logo: "",
+    headerImage: "",
   },
   {
-    categoryId: "history",
-    displayName: "History",
-    description: "All History quizes.",
-    image: "",
+    categoryId: "common-knowledge",
+    displayName: "Yleis tieto",
+    description:
+      "Luulitko tietäväsi perusjutut? Nämä kysymykset kattavat kaiken, mitä jokaisen pitäisi tietää",
+    image: "pub-quiz.jpg",
+    logo: "",
+    headerImage: "",
   },
+  // {
+  //   categoryId: "history",
+  //   displayName: "History",
+  //   description: "All History quizes.",
+  //   image: "pub-quiz.jpg",
+  // },
 ];
--- application/app-infra/lambdas/Api/src/mock/games.ts
+++ application/app-infra/lambdas/Api/src/mock/games.ts
@@ -1,15 +1,47 @@
 export const games = [
   {
-    gameId: "world-cup-quiz",
-    categoryId: "sports",
-    title: "World Cup quiz",
-    image: "2020.jpg",
-    createdAt: "2025-04-13T15:00:00Z",
+    gameId: "light-questions",
+    categoryId: "sport",
+    title: "Urheilutietovisailu: Kevyt kysymyspaketti",
+    description:
+      "Täydellisiä kysymyksiä rentoon pubi visailuun, sopii iltamyöhälle kuin myös iltapäivälle",
+    image: "sport-light-questions.jpg",
+    pngImage: "sport-light-questions.png",
     questions: [
       {
         image: "/games/bg-1.webp",
-        text: "How many people live in Finland?",
-        answer: "400 000",
+        text: "Kuinka monta pelaajaa on jalkapallojoukkueessa kentällä samaan aikaan?",
+        answer: "1",
+      },
+      {
+        image: "/games/bg-1.webp",
+        text: "Mikä maa tunnetaan jääkiekon suurvaltana ja on voittanut useita maailmanmestaruuksia?",
+        answer: "Kanada",
+      },
+      {
+        image: "/games/bg-1.webp",
+        text: "Kuinka monta rengasta on olympialaisten tunnuksessa?",
+        answer: "5",
+      },
+      {
+        image: "/games/bg-1.webp",
+        text: "Missä urheilulajissa voi saada hole-in-one?",
+        answer: "Golf",
+      },
+      {
+        image: "/games/bg-1.webp",
+        text: "Kuka suomalainen on voittanut useita F1-maailmanmestaruuksia?",
+        answer: "Mika Häkkinen",
+      },
+      {
+        image: "/games/bg-1.webp",
+        text: "Mikä on Suomen kansallisurheilulaji?",
+        answer: "Pesäpallo",
+      },
+      {
+        image: "/games/bg-1.webp",
+        text: "Mikä on jalkapallon suurin kansainvälinen turnaus?",
+        answer: "Fifa World Cup",
       },
     ],
   },
