f3bc3dd8e6e4bde6473f4b3c82f63a9c0f507a2e
Christian <Christian>
2025-06-03 22:51:59 +0300

    small updates

--- application/angular-app/src/app/game-category/gc-navbar/gc-navbar.component.ts
+++ application/angular-app/src/app/game-category/gc-navbar/gc-navbar.component.ts
@@ -1,20 +1,25 @@
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { LogoComponent } from '../../../shared/components/logo/logo.component';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { getSelectedGame } from '../../../store/selectors/games.selectors';
 import { getSelectedGameCategory } from '../../../store/selectors/game-categories.selectors';
 
 @Component({
   standalone: true,
   selector: 'app-gc-navbar',
-  imports: [LogoComponent, MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatButtonModule, MatIconModule, RouterModule],
   templateUrl: 'gc-navbar.component.html',
   styleUrl: 'gc-navbar.component.scss',
 })
 export class GCNavbarComponent {
   store = inject(Store);
   selectedGameCategory = this.store.selectSignal(getSelectedGameCategory);
+
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- application/angular-app/src/app/game/navbar/game-navbar.component.ts
+++ application/angular-app/src/app/game/navbar/game-navbar.component.ts
@@ -1,20 +1,25 @@
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { LogoComponent } from '../../../shared/components/logo/logo.component';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { getSelectedGame } from '../../../store/selectors/games.selectors';
 import { getSelectedGameCategory } from '../../../store/selectors/game-categories.selectors';
 
 @Component({
   standalone: true,
   selector: 'app-game-navbar',
-  imports: [LogoComponent, MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatButtonModule, MatIconModule, RouterModule],
   templateUrl: 'game-navbar.component.html',
   styleUrl: 'game-navbar.component.scss',
 })
 export class GameNavbarComponent {
   store = inject(Store);
   selectedGameCategory = this.store.selectSignal(getSelectedGameCategory);
+
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
 }
